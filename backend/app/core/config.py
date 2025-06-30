1b5621ec26c7afaf6cae1877a5a7978967a71baf
Aziz Dogan <93579461+Shrupe@users.noreply.github.com>
2025-06-30 22:11:48 +0300

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -8,13 +8,6 @@ HOST=os.getenv("HOST")
 PORT=os.getenv("PORT")
 DB=os.getenv("DB")
 USER=os.getenv("USER")
-                                       
-PASSWORD="986532"
-HOST="localhost"
-PORT="5432"
-DB="coolweb"
-USER="postgres"
-
-
+                                      
 URL = f"postgresql+asyncpg://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
 DATABASE_URL = os.getenv("DATABASE_URL", URL)
